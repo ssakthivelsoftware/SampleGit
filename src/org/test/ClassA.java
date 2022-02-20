5fa7b6528fcc2a692cb49dd8caeb2a2a8eaaebb6
ssakthivelsoftware <sakthivels912@gmail.com>
2022-02-20 17:39:54 +0530

    gitfirst

---
+++ src/org/test/ClassA.java
@@ -0,0 +1,17 @@
+package org.test;
+
+public class ClassA {
+
+	public void test1() {
+		System.out.println("test 1");
+	}
+	public void test2() {
+		System.out.println("test 2");
+	}
+	public static void main(String[] args) {
+		ClassA sample= new ClassA();
+		sample.test1();
+		sample.test2();
+		
+	}
+}
